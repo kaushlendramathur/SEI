94b3ca0cb9b003b37444647fa636da35e31c8b39
amdpt323 <samandeep21@iitk.ac.in>
2024-08-16 10:21:54 +0530

    added about page

--- src/components/screens/About.tsx
+++ src/components/screens/About.tsx
@@ -1,16 +1,104 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView, ImageBackground, ImageSourcePropType, StyleSheet, Dimensions, Pressable } from 'react-native';
 import { NavigationProp } from '@/types/interfaces';
+import About1 from '@/assets/images/about-1.png'
+import About2 from '@/assets/images/about-2.png'
+import About3 from '@/assets/images/about-3.png'
+import {aboutText} from '@/constants/static'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faArrowLeft, faLeftLong } from '@fortawesome/free-solid-svg-icons';
+
+const {height,width} = Dimensions.get('screen');
+
+const Highlights = ({bold,plain}:{bold:string[],plain:string[]})=>{
+  return (
+    <View>
+      {bold.map((text, index) => {
+        return (
+          <View key={index} style={styles.highlights}>
+            <Text>
+              <Text style={{ fontWeight: 'bold' }}>{text} : </Text>
+              {plain[index]}
+            </Text>
+          </View>
+        )
+      })}
+    </View>
+  )
+}
 
 const About: React.FC<NavigationProp> = ({ navigate }) => {
   return (
-    <View>
-      <Text>About</Text>
-      <TouchableOpacity onPress={() => navigate('Profile')}>
-        <Text>Go to Profile</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    <ScrollView style={styles.container}>
+
+      <ImageBackground
+        source={About1 as ImageSourcePropType}
+        style={{ height: 200 }}
+        imageStyle={styles.imageStyle}
+      >
+        <Pressable onPress={()=>navigate('Profile')}>
+          <FontAwesomeIcon icon={faLeftLong} style={styles.arrow}/>
+        </Pressable>
+      </ImageBackground>
+      <View style={styles.textContainer}>
+        <Text style={{ fontSize: 30, fontWeight: 300, paddingBottom: 10 }}>
+          About Us
+        </Text>
+        <Text>{aboutText.main}</Text>
+        <ImageBackground
+          source={About2 as ImageSourcePropType}
+          style={{ height: 400, marginVertical: 30 }}
+          imageStyle={styles.imageStyle}
+        />
+        <Text style={{ fontSize: 30, fontWeight: 300, paddingBottom: 10 }}>
+          Our Mission
+        </Text>
+        <Text>{aboutText.misson}</Text>
+        <Text style={{ fontSize: 30, fontWeight: 300, paddingVertical: 10 }}>
+          Our Vision
+        </Text>
+        <Text>
+          {aboutText.vision.intro}
+          {aboutText.vision.points.map((point, index) => {
+            return (
+              <View key={index} style={{ flexDirection: 'row',paddingTop:5}}>
+                <Text style={{ fontWeight: 'bold' }}>{index + 1} . </Text>
+                <Text>{point}</Text>
+              </View>
+            )
+          })}
+        </Text>
+        <ImageBackground
+          source={About3 as ImageSourcePropType}
+          style={{ height: 500, marginVertical: 30 }}
+          imageStyle={styles.imageStyle}
+        />
+      </View>
+      <Highlights bold={aboutText.bold} plain={aboutText.plain} />
+    </ScrollView>
+  )
 };
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor:'white',
+  },
+  textContainer:{
+    padding:30,
+  },
+  imageStyle:{
+    resizeMode:'cover',
+  },
+  arrow:{
+    color:'white',
+    marginTop:-5,
+    padding:15,
+  },
+  highlights:{
+    paddingHorizontal:30,
+    paddingVertical:5, 
+
+  }
+})
 
 export default About;
