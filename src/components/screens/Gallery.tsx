64c8403e31e367a81dfee2ac1274169e446d6cbb
aahirwar20 <aahirwar20@iitk.ac.in>
2024-08-21 17:08:18 +0530

    add gallery page

--- src/components/screens/Gallery.tsx
+++ src/components/screens/Gallery.tsx
@@ -1,16 +1,58 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Image, ScrollView, StyleSheet, View, Dimensions, Text, Pressable } from 'react-native';
+import { images, ImageNames } from '@/utils/importImages';
 import { NavigationProp } from '@/types/interfaces';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
 
-const Gallery: React.FC<NavigationProp> = ({ navigate }) => {
+
+const ImageGallery: React.FC<NavigationProp> = ({navigate}) => {
   return (
-    <View>
-      <Text>Gallery</Text>
-      <TouchableOpacity onPress={() => navigate('Profile')}>
-        <Text>Go to Profile</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Pressable onPress={() => navigate('Profile')}>
+          <FontAwesomeIcon icon={faLeftLong} style={styles.arrow} />
+      </Pressable>
+
+      <Text style={styles.title}>Gallery</Text>
+
+      {(Object.keys(images) as ImageNames[]).map((imageName, index) => (
+        <View key={index} style={styles.imageWrapper}>
+          <Image
+            source={images[imageName]}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
-export default Gallery;
+const styles = StyleSheet.create({
+  container: {
+    padding: 10, 
+  },
+  imageWrapper: {
+    marginBottom: 20, 
+  },
+  image: {
+    width: Dimensions.get('window').width - 20, 
+    height: 200,
+    resizeMode: 'cover'
+  },
+  arrow:{
+    color:'gray',
+    marginTop:0,
+    padding:15,
+  },
+  title: {
+    color: 'black',
+    fontSize: 24,
+    fontWeight: 'semibold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  
+});
+
+export default ImageGallery;
