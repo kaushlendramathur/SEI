a4aed275a7be3f5322f1dc1b125b98e05d9c0c0e
aahirwar20 <aahirwar20@iitk.ac.in>
2024-08-27 11:25:46 +0530

    fix about page

--- src/components/screens/About.tsx
+++ src/components/screens/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, ScrollView, ImageBackground, ImageSourcePropType, StyleSheet, Dimensions, Pressable } from 'react-native';
+import { Text, View, TouchableOpacity, ScrollView, ImageBackground, Image,ImageSourcePropType, StyleSheet, Dimensions, Pressable } from 'react-native';
 import { NavigationProp } from '@/types/interfaces';
 import About1 from '@/assets/images/about-1.png'
 import About2 from '@/assets/images/about-2.png'
@@ -17,7 +17,7 @@ const Highlights = ({bold,plain}:{bold:string[],plain:string[]})=>{
       {bold.map((text, index) => {
         return (
           <View key={index} style={styles.highlights}>
-            <Text style={{ textAlign: 'justify' }}>
+            <Text style={{ textAlign: 'justify', fontSize:14, fontWeight: '300' }}>
               <Text style={{ fontWeight: 'bold' }}>{text} : </Text>
               {plain[index]}
             </Text>
@@ -28,13 +28,16 @@ const Highlights = ({bold,plain}:{bold:string[],plain:string[]})=>{
   )
 }
 
+
+
 const About: React.FC<NavigationProp> = ({ navigate }) => {
+
+  
   return (
     <ScrollView style={styles.container}>
       <ImageBackground
         source={About1 as ImageSourcePropType}
         style={{ height: 200 }}
-        imageStyle={styles.imageStyle}
       >
         <Pressable onPress={() => navigate('Profile')}>
           <FontAwesomeIcon icon={faLeftLong} style={styles.arrow} />
@@ -57,29 +60,40 @@ const About: React.FC<NavigationProp> = ({ navigate }) => {
         <Text style={{ fontSize: 30, fontWeight: 300, paddingVertical: 10 }}>
           Our Vision
         </Text>
-        <Text style={{ textAlign: 'justify' }}>
-          {aboutText.vision.intro}
-          {`\n`}
+        <View>
+          <Text style={{ textAlign: 'justify' }}>
+            {aboutText.vision.intro}
+          </Text>
           {aboutText.vision.points.map((point, index) => {
             return (
               <View key={index} style={{ flexDirection: 'row', paddingTop: 5 }}>
-                <Text style={{ fontWeight: 'bold' }}>{index + 1} . </Text>
-                <Text style={{ textAlign: 'justify', paddingRight:30}}>{point}</Text>
+                <Text style={{ fontWeight: 'bold' }}>{index + 1}. </Text>
+                <Text style={{ textAlign: 'justify', paddingRight: 0, flex: 1 }}>{point}</Text>
               </View>
-            )
+            );
           })}
-        </Text>
+        </View>
+
         <ImageBackground
           source={About3 as ImageSourcePropType}
           style={{ height: 500, marginVertical: 30 }}
           imageStyle={styles.imageStyle}
         />
         <Highlights bold={aboutText.bold} plain={aboutText.plain} />
-        <ImageBackground
-          source={About4 as ImageSourcePropType}
-          style={{ height: 500, marginVertical: 30 }}
-          imageStyle={styles.imageStyle}
-        />
+       
+        <View>
+          <Image
+             source={About4 as ImageSourcePropType}
+             style={{width:'100%', marginVertical:20}}
+          />
+          <View style={styles.overlay}>
+            <Text style={styles.smallText}>Certificate by Lloyd's Register Marine and Offshore India LLP</Text>
+            <TouchableOpacity style={styles.largeButton}>
+              <Text style={styles.buttonText}>Large Button</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
       </View>
     </ScrollView>
   )
@@ -92,8 +106,8 @@ const styles = StyleSheet.create({
   textContainer:{
     padding:30,
   },
-  imageStyle:{
-    resizeMode:'cover',
+  imageStyle: {
+    borderRadius: 15, // Optional: If you want rounded corners for the image
   },
   arrow:{
     color:'white',
@@ -102,7 +116,28 @@ const styles = StyleSheet.create({
   },
   highlights:{
     paddingVertical:5, 
-  }
+  },
+  overlay: {
+    alignItems: 'center',
+  },
+  smallText: {
+    fontSize: 16,
+    marginBottom: 20, // Space between text and button
+    textAlign: 'center', // Center text
+  },
+  largeButton: {
+    backgroundColor: 'black', // Button color
+    paddingVertical: 15,
+    width: 200,
+    textAlign:'center',
+    alignItems:'center',
+    borderRadius: 10, // Optional: Rounded corners for the button
+  },
+  buttonText: {
+    color: '#fff', // Text color for the button
+    fontSize: 18, // Large font size
+    fontWeight: 'bold',
+  },
 })
 
 export default About;
