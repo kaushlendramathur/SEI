9552b850f1c81c0eac733b21d15a87f77331f025
aahirwar20 <aahirwar20@iitk.ac.in>
2024-09-03 17:56:34 +0530

    fix the forget click glitch and set username

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -120,7 +120,7 @@ const Login = () => {
           </Pressable>
           <Text>Remember Me</Text>
 
-          <Link href='/login' style={styles.forgotPassword}>
+          <Link href='/forget' style={styles.forgotPassword}>
             Forgot Password?
           </Link>
         </View>
