98efb144ca522daeca72b93b8f16b58a4aa5b2b6
amdpt323 <95157589+amdpt323@users.noreply.github.com>
2024-09-03 11:49:32 +0530

    Added PDF VIEWER for REFUND , PRIVACY AND TNC (#17)
    * fixed terms refund privacy
    
    * make compatible for android device
    
    ---------
    
    Co-authored-by: aahirwar20 <aahirwar20@iitk.ac.in>

--- src/components/screens/Terms.tsx
+++ src/components/screens/Terms.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { NavigationProp } from '@/types/interfaces';
+import PdfViewer from '../PdfViewer';
 
 const Terms: React.FC<NavigationProp> = ({ navigate }) => {
   return (
     <View>
-      <Text>Terms</Text>
-      <TouchableOpacity onPress={() => navigate('Profile')}>
-        <Text>Go to Profile</Text>
-      </TouchableOpacity>
+      <View>
+        <PdfViewer
+          uri={
+            'https://www.seiedutrust.com/Content/images/1-TERMS%20AND%20CONDITIONS.pdf'
+          }
+        />
+      </View>
     </View>
-  );
+  )
 };
 
 export default Terms;
